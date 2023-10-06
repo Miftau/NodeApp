d820ab6923b98a3ac19daa384f114bddc349de6e
Miftau <miftaudeenb@gmail.com>
2023-10-06 12:15:54 +0100

    routers constructed

--- app.js
+++ app.js
@@ -1,8 +1,48 @@
 const express = require('express')
 
 const app = express()
-const port = 3000
+const port = 4000
+
+// middleware for static files
+app.use(express.static(`./public`))
+
+// middleware for getting for data
+
+app.use(express.urlencoded({extended: true}))
 
 app.listen(port, ()=> {
-    console.log('Server running on port 3000')
+    console.log(`Server running on port ${port}`)
+})
+
+// home route
+app.get('/',(req, res)=> {
+    res.sendFile(__dirname, 'index.html')
+})
+
+// post route
+
+app.post(`./submit`, (req, res)=> {
+    const username = req.body.username
+    const email = req.body.email
+    console.log(req.body)
+
+    req.send(`
+           Your username is ${username} and email is ${eamil}
+    `)
+})
+
+app.get(`/about`,(req, res)=> {
+    res.send(`<h1> You are welcome to my NodeApp</h1>`)
+})
+
+app.get(`/student`,(req, res)=> {
+    res.send(`<h1> Mr Progress</h1>
+              <h1> Mr Yusuf</h1>
+              <h1> Quadtech </h1>
+              <h1> Babatunde </h1>
+    `)
+})
+
+app.all(`*`, (req, res)=> {
+    res.send(`<h1> PAGE NOT FOUNG! 404 </h1>`)
 })
